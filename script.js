f09cea1935391eabb1d918ff8e0747832ede4dc1
Louis <louiscdb@icloud.com>
2025-04-27 22:50:19 +0200

    split the project into multiple files in classes.js i added animatedwindow whic gets the function newcanvasecontainter as parameter so it dynamically makes a canvas element (which will later be used when onclick event listeners get called)

--- script.js
+++ script.js
@@ -5,20 +5,45 @@ source.src = 'BG_video.mp4';
 source.type = 'video/mp4';
 video.appendChild(source);
 video.load();
-
+var clicks=0
+document.addEventListener('click', function(event) {
+    clicks+=1;
+    const mouseX = event.clientX; // X position inside the viewport
+    const mouseY = event.clientY; // Y position inside the viewport
+    new animatedwindow(0,0,100,500,10,10,"blue",newCanvasContainter("test"+clicks,mouseX,mouseY));
+});
+/*
 const titles=document.querySelectorAll("h1");
 titles.forEach((title,index)=>{
-    moveItems(title,-500,1000,0,50);
-});
+    moveItems(title,-10000,1000,0,50);
+});*/
 
-function moveItems(pItem,percentX,percentY,pFrame,pSteps)
-{
 
-    let currentPercent=getTranslatePercent(pItem);
-    let vector = [
-        currentPercent.x + (percentX / pSteps),
-        currentPercent.y + (percentY / pSteps)
-    ];
+
+//const newdiv=newCanvasContainter("blabla");
+//const test=new animatedwindow(10,10,100,500,10,1,"blue",newdiv);
+//const test= new animatedwindow(10,50,100,500,10,10,"blue",newCanvasContainter("test"))
+//const context=document.getElementById('test-canvas').getContext("2d");
+//const context=test.getCanvas().getContext('2d')
+//context.fillRect(20,20,200,200);
+//console.log(context);
+
+function moveItems(pItem, percentX, percentY, pFrame, pSteps) {
+    const currentPercent = getTranslatePercent(pItem);
+
+    const vectorX = (percentX - currentPercent.x) / pSteps;
+    const vectorY = (percentY - currentPercent.y) / pSteps;
+
+    if (pFrame < pSteps) {
+        moveByPercent(pItem, currentPercent.x + vectorX, currentPercent.y + vectorY);
+        requestAnimationFrame(() => moveItems(pItem, percentX, percentY, pFrame + 1, pSteps));
+    } else {
+        moveByPercent(pItem, percentX, percentY); // Ensure it reaches the target
+    }
+}
+
+function moveByPercent(pItem, targetX, targetY) {
+    pItem.style.transform = `translate(${targetX}%, ${targetY}%)`;
 }
 
 function getTranslatePercent(elem) {
@@ -42,20 +67,16 @@ function getTranslateValues(elem) {
         y: matrix.m42  // translateY in pixels
     };
 }
-function moveBypercent(pItem,pTarget)
-{
-    pItem.style.transition = 'transform 0.1s ease-in-out';
-    pItem.style.transform = `translate(${pTarget[0]}%, ${pTarget[1]}%)`;
-
-}
 
 
-function newCanvasContainter(pId)
+function newCanvasContainter(pId,x,y)
 {
     const containerDiv = document.createElement('div');
     containerDiv.id = 'div'+pId;
     document.body.appendChild(containerDiv);
-
+    containerDiv.style.position = 'absolute';
+    containerDiv.style.left = x + 'px';
+    containerDiv.style.top = y + 'px';
     // Applying flex styling to the container div
     containerDiv.style.display = 'flex';
     containerDiv.style.flexWrap = 'wrap';
@@ -69,10 +90,4 @@ function newCanvasContainter(pId)
 }
 import {animatedwindow} from './classes.js'
 
-//const newdiv=newCanvasContainter("blabla");
-//const test=new animatedwindow(10,10,100,500,10,1,"blue",newdiv);
-const test= new animatedwindow(10,50,100,500,10,10,"blue",newCanvasContainter("test"))
-//const context=document.getElementById('test-canvas').getContext("2d");
-const context=test.getCanvas().getContext('2d')
-//context.fillRect(20,20,200,200);
-console.log(context);
+
