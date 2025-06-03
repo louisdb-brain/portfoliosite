c409ca3c2ab7d9541b161950e9a44fb6e8ab95bc
louisdb-brain <louiscdb@icloud.com>
2025-06-03 15:03:43 +0200

    Create 3dmanager.js

---
+++ 3dmanager.js
@@ -0,0 +1,19 @@
+export class 3dmanager{
+constructor (scene){
+    this.scene=scene
+    this.model='brainfile.obj'
+    this.position=new Three.vector3(()
+    this rotation= new three.vector3()
+    this.scrollDistance=0
+    this.pageheight=2000
+    
+    }
+    update(ctx){
+        const
+        
+    
+        
+        }
+
+
+}
